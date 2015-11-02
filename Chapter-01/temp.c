8af54702ffc83dad7d10770f2bf256b6a87c8920
James Reinke <james@rtsystems.io>
2015-11-02 13:46:26 -0800

    ~

--- Chapter-01/temp.c
+++ Chapter-01/temp.c
@@ -4,7 +4,7 @@
 	Print Fahrenheit-Celsisus table
 */
 int main(){
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower, upper, step;
 
 	lower = 0;
@@ -13,8 +13,8 @@ int main(){
 
 	fahr = lower;
 	while(fahr <= upper) {
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = (5.0 / 9.0) * (fahr - 32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}	
 }
